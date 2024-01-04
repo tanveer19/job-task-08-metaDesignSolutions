531fbe6fc85a67bc47802cd7f8f738a131403d82
Tanveer Hossain Jony <thjbd19@gmail.com>
2024-01-04 21:41:26 +0600

    delete works

--- main.js
+++ main.js
@@ -1,1 +1,79 @@
 import "./style.scss";
+
+var modal = document.getElementById("exampleModal");
+var saveButton = modal.querySelector(".modal-footer .btn-primary");
+
+saveButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  var dob = document.getElementById("dob").value;
+  var fullName = document.getElementById("fullName").value;
+  var nickName = document.getElementById("nickName").value;
+  // Create a new Date object from the dob value
+  var date = new Date(dob);
+
+  // Define an array of month names
+  var monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  // Format the date as "Aug 27, 2020"
+  var formattedDate =
+    monthNames[date.getMonth()] +
+    " " +
+    date.getDate() +
+    ", " +
+    date.getFullYear();
+
+  var activityDiv = document.createElement("div");
+  activityDiv.classList.add(
+    "activity",
+    "border",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+  );
+
+  // Create a div for the activity details
+  var activityDetails = document.createElement("div");
+  activityDetails.innerHTML =
+    fullName + "<br />" + formattedDate + "  " + " | " + "  " + nickName;
+
+  // Add the activity details to the activity div
+  activityDiv.appendChild(activityDetails);
+
+  // Create the dropdown menu
+  var dropdownMenu = document.createElement("div");
+  dropdownMenu.classList.add("dropdown");
+  dropdownMenu.innerHTML = `
+      <i class="fa-solid fa-ellipsis-vertical" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false"></i>
+      <ul class="dropdown-menu " aria-labelledby="dropdownMenuButton">
+        <li><a href="#" class="dropdown-item" id="markAsDone">Mark as Done</a></li>
+        <li><a href="#" class="dropdown-item" id="delete">Delete</a></li>
+      </ul>
+    `;
+
+  // Add the dropdown menu to the activity div
+  activityDiv.appendChild(dropdownMenu);
+
+  dropdownMenu.querySelector("#delete").addEventListener("click", function () {
+    activityDiv.remove();
+  });
+
+  document.getElementById("activities").appendChild(activityDiv);
+
+  // Close the modal
+  var bootstrapModal = bootstrap.Modal.getInstance(modal);
+  bootstrapModal.hide();
+});
